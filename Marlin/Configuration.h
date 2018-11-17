3101fc8d6f98fa124fbed55b661babc9a425537e
David Ramiro <david@kore.cc>
2018-11-17 19:53:29 +0100

    Raising maximum hotend temperature
    Due to a request, I am raising the maximum temperature allowed on the hotend more flexibility on ABS and PETG printing.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -350,7 +350,7 @@
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
 // You should use MINTEMP for thermistor short/failure protection.
-#define HEATER_0_MAXTEMP 245
+#define HEATER_0_MAXTEMP 255
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
