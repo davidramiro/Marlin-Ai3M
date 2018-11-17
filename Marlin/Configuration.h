c583398198d4bb065d24f9830954aac1fbfac8eb
David Ramiro <david@kore.cc>
2018-11-17 20:19:33 +0100

    Correcting bed size
    Migrating bed size values from manufacturer firmware

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -890,9 +890,9 @@
 #define X_MIN_POS -5
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 205
+#define X_MAX_POS 215
+#define Y_MAX_POS 215
+#define Z_MAX_POS 206
 
 /**
  * Software Endstops
