4fd3347f81084c1dab1ac681af26a60d026cd0ed
David Ramiro <david@kore.cc>
2018-11-24 03:38:15 +0100

    Enable G26 mesh validation
    Enabling the G26 command for easy testing of the bed mesh removing the need of a manually printed bed level test file.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,4 +1,5 @@
 /**
+/**
  * Marlin 3D Printer Firmware
  * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
@@ -1011,7 +1012,7 @@
   /**
    * Enable the G26 Mesh Validation Pattern tool.
    */
-  //#define G26_MESH_VALIDATION
+#define G26_MESH_VALIDATION
   #if ENABLED(G26_MESH_VALIDATION)
     #define MESH_TEST_NOZZLE_SIZE    0.4  // (mm) Diameter of primary nozzle.
     #define MESH_TEST_LAYER_HEIGHT   0.2  // (mm) Default layer height for the G26 Mesh Validation Tool.
