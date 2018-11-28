e527750d816a4fc64a3dd22d63f1557c7dd4f67d
David Ramiro <david@kore.cc>
2018-11-28 23:33:11 +0100

    Enable S-Curve Acceleration and tweak jerk
    To further optimize print quality even at very high speeds (I aim for roughly 80-100mm/s), I enabled S_CURVE_ACCELERATION and tweaked the jerk and default acceleration values.
    
    Note: This is still in testing. As soon as I finish and it works as I like, I'll push it onto the master branch. If you see this commit on the master branch, it's safe to flash.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -637,7 +637,7 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
 
@@ -649,8 +649,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 10.0
-#define DEFAULT_YJERK                 10.0
+#define DEFAULT_XJERK                 8.0
+#define DEFAULT_YJERK                 8.0
 #define DEFAULT_ZJERK                  0.4
 #define DEFAULT_EJERK                  5.0
 
@@ -662,7 +662,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
