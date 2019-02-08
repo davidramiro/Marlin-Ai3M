f01026e74866b2147954d677e8d8ba0f43e2e69e
David Ramiro <david@kore.cc>
2019-02-08 16:28:02 +0100

    Tweak minimum segment time
    Improve USB printing by slowing down on certain situations. This prevents the buffer from running empty and removes the need to wait for new serial commands.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -431,7 +431,7 @@
 // @section extras
 
 // minimum time in microseconds that a movement needs to take if the buffer is emptied.
-#define DEFAULT_MINSEGMENTTIME        20000
+#define DEFAULT_MINSEGMENTTIME        50000
 
 // If defined the movements slow down when the look ahead buffer is only half full
 #define SLOWDOWN
